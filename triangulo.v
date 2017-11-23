f1f19b8b8103ec0c7bc1e87ae111e7c5371cbaf8
leoojg <31110934+leoojg@users.noreply.github.com>
2017-11-23 15:44:39 -0200

    Update triangulo.v

--- triangulo.v
+++ triangulo.v
@@ -9,13 +9,12 @@ module triangulo(
    
    reg [10:0] 		      cx = 0;
    reg [9:0] 		      cy = 0;		      
-   reg [10:0] 		      ponto1_x = 400;
-   reg [9:0] 		      ponto1_y = 80;
-   reg [10:0] 		      ponto2_x = 700;
-   reg [9:0] 		      ponto2_y = 500;
-   reg [10:0] 		      ponto3_x = 1200;
-   reg [9:0] 		      ponto3_y = 450;
-
+   reg [10:0] 		      ponto1_x = 300;
+   reg [9:0] 		      ponto1_y = 100;
+   reg [10:0] 		      ponto2_x = 400;
+   reg [9:0] 		      ponto2_y = 300;
+   reg [10:0] 		      ponto3_x = 600;
+   reg [9:0] 		      ponto3_y = 200;
 
    
    assign VGA_R = v ? (t ? 4'hc : 4'h0) : 4'b0;
@@ -23,14 +22,15 @@ module triangulo(
    assign VGA_B = v ? (t ? 4'hc : 4'h0) : 4'b0;
    
    wire 		      v = (cx >= 285) & (cx < 1555) & (cy >= 35) & (cy < 515);
-   wire 		      t = ((ponto2_x - ponto1_x) * (cy - ponto1_y) - (cx - ponto1_x) * (ponto2_y - ponto1_y) ) <= 0 &&
-			      ((ponto3_x - ponto2_x) * (cx - ponto2_x) - (cy - ponto2_y) * (ponto3_y - ponto2_y) ) <= 0 &&
-			      ((ponto1_x - ponto3_x) * (cy - ponto3_y) - (cx - ponto3_x) * (ponto1_y - ponto3_y) ) <= 0;
+   wire 		      t = ((ponto2_x - ponto1_x) * (cy - ponto1_y) - (cx - ponto1_x) * (ponto2_y - ponto1_y) ) <= 0 &
+ 				  ((ponto3_x - ponto2_x) * (cy - ponto2_y) - (cx - ponto2_x) * (ponto3_y - ponto2_y) ) <= 0 &
+				  ((ponto1_x - ponto3_x) * (cy - ponto3_y) - (cx - ponto3_x) * (ponto1_y - ponto3_y) ) <= 0;
    reg [10:0] 		      c = 10;
    assign VGA_HS = cx >= 190;
    assign VGA_VS = cy >= 2;
    
    always @(posedge CLOCK_50) begin
+	
       if (cx == 1585) 
 	begin
            if (cy == 525) begin
