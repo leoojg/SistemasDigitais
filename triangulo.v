590d8ca8fcb40195070b232691a53049ab49b1b4
leoojg <31110934+leoojg@users.noreply.github.com>
2017-12-14 14:58:02 -0200

    Update triangulo.v

--- triangulo.v
+++ triangulo.v
@@ -7,14 +7,14 @@ module triangulo(
  		 output       VGA_HS,
 		 output       VGA_VS);
    
-   reg [10:0] 		      cx = 0;
-   reg [9:0] 		      cy = 0;		      
-   reg [10:0] 		      p1_x = 300;
-   reg [9:0] 		      p1_y = 100;
-   reg [10:0] 		      p2_x = 400;
-   reg [9:0] 		      p2_y = 300;
-   reg [10:0] 		      p3_x = 600;
-   reg [9:0] 		      p3_y = 200;
+   reg signed [25:0] 		      cx = 0;
+   reg signed [25:0] 		      cy = 0;		      
+   reg signed [25:0] 		      p1_x = 300;
+   reg signed [25:0] 		      p1_y = 100;
+   reg signed [25:0] 		      p2_x = 400;
+   reg signed [25:0] 		      p2_y = 300;
+   reg signed [25:0] 		      p3_x = 600;
+   reg signed [25:0] 		      p3_y = 200;
 
    
    assign VGA_R = v ? (tr ? 4'hc : 4'h0) : 4'b0;
@@ -24,13 +24,13 @@ module triangulo(
    
    
    
-   wire 		      v = (cx >= 285) & (cx < 1555) & (cy >= 35) & (cy < 515);
-   wire 		      tr;
-
-   t teste(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y, cx, cy, tr);
+   wire 			      v = (cx >= 285) & (cx < 1555) & (cy >= 35) & (cy < 515);
+   wire 			      tr = ((p2_x - p1_x) * (cy - p1_y) -(cx - p1_x) * (p2_y - p1_y)) <= 0 &&
+				              ((p3_x - p2_x) * (cy - p2_y) -(cx - p2_x) * (p3_y - p2_y)) <= 0 &&
+				              ((p1_x - p3_x) * (cy - p3_y) -(cx - p3_x) * (p1_y - p3_y)) <= 0;
+     
    
-			      
-   reg [10:0] 		      c = 10;
+   reg [10:0] 			      c = 10;
    assign VGA_HS = cx >= 190;
    assign VGA_VS = cy >= 2;
    
